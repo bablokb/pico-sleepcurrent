e8cba876a92ce90d66d70519d83642c3ae71ee1e
Bernhard Bablok <bablokb@gmx.de>
2025-01-23 12:39:54 +0100

    increase work-time after light-sleep (test for preserved state)

--- cp-pin/main.py
+++ cp-pin/main.py
@@ -11,12 +11,15 @@ import board
 import alarm
 from digitalio import DigitalInOut, Direction, Pull
 
-LED_TIME = 1
-WAKE_PIN = board.GP16
+LED_TIME    = 1
+WORK_TIME   = 10
+WAKE_PIN    = board.GP16
 
-LIGHT_SLEEP  = 0x3
-DEEP_SLEEP   = 0x4
-MODE         = DEEP_SLEEP
+LIGHT_SLEEP = 0x3
+DEEP_SLEEP  = 0x4
+MODE        = LIGHT_SLEEP
+
+# --- create objects   --------------------------------------------------------
 
 if hasattr(board,'NEOPIXEL'):
   import neopixel_write
@@ -32,15 +35,20 @@ else:
 # --- simulate work   --------------------------------------------------------
 
 def work():
-  if hasattr(board,'NEOPIXEL'):
-    led_power.value = 1
-    neopixel_write.neopixel_write(led,led_value)
+  print(f"working for {WORK_TIME}s, blinking every {LED_TIME}s ...")
+  start = time.monotonic()
+  end = start + WORK_TIME-2*LED_TIME
+  while time.monotonic() < end:
+    if hasattr(board,'NEOPIXEL'):
+      led_power.value = 1
+      neopixel_write.neopixel_write(led,led_value)
+      time.sleep(LED_TIME)
+      led_power.value = 0
+    else:
+      led.value = 1
+      time.sleep(LED_TIME)
+      led.value = 0
     time.sleep(LED_TIME)
-    led_power.value = 0
-  else:
-    led.value = 1
-    time.sleep(LED_TIME)
-    led.value = 0
 
 # --- main loop   ------------------------------------------------------------
 
@@ -48,6 +56,10 @@ while True:
   work()
   pin_alarm = alarm.pin.PinAlarm(WAKE_PIN,value=False,edge=True,pull=True)
   if MODE == LIGHT_SLEEP:
+    print(f"light-sleeping until pin-alarm ...")
     alarm.light_sleep_until_alarms(pin_alarm)
+    WORK_TIME *= 1.5
   elif MODE == DEEP_SLEEP:
+    print(f"deep-sleeping until pin-alarm ...")
     alarm.exit_and_deep_sleep_until_alarms(pin_alarm)
+    WORK_TIME *= 1.5    # this should have no effect due to reset!
--- cp-timer/main.py
+++ cp-timer/main.py
@@ -9,11 +9,18 @@
 import time
 import board
 import microcontroller
-import alarm
+try:
+  import alarm
+  flip = int(alarm.sleep_memory[0] > 0)
+except:
+  print("warning: no alarm-module: only SPIN/SLEEP-tests available")
+  flip = 0
+
 from digitalio import DigitalInOut, Direction, Pull
 
 LED_TIME     = 1
-INT_TIME     = 30 - LED_TIME
+WORK_TIME    = 10
+INT_TIME     = 30 - WORK_TIME
 
 SPIN         = 0x1
 SLEEP        = 0x2
@@ -21,9 +28,9 @@ LIGHT_SLEEP  = 0x3
 DEEP_SLEEP   = 0x4
 MODE         = LIGHT_SLEEP
 
-CPU_FREQ_DEF = 125000000
-CPU_FREQ     =  40000000
-#CPU_FREQ     = None
+CPU_FREQ_DEF = microcontroller.cpu.frequency
+CPU_FREQ     =  25_000_000
+CPU_FREQ     = None
 
 # --- create objects   --------------------------------------------------------
 
@@ -41,21 +48,25 @@ else:
 # --- simulate work   --------------------------------------------------------
 
 def work():
-  if hasattr(board,'NEOPIXEL'):
-    led_power.value = 1
-    neopixel_write.neopixel_write(led,led_value)
+  print(f"working for {WORK_TIME}s, blinking every {LED_TIME}s ...")
+  start = time.monotonic()
+  end = start + WORK_TIME-2*LED_TIME
+  while time.monotonic() < end:
+    if hasattr(board,'NEOPIXEL'):
+      led_power.value = 1
+      neopixel_write.neopixel_write(led,led_value)
+      time.sleep(LED_TIME)
+      led_power.value = 0
+    else:
+      led.value = 1
+      time.sleep(LED_TIME)
+      led.value = 0
     time.sleep(LED_TIME)
-    led_power.value = 0
-  else:
-    led.value = 1
-    time.sleep(LED_TIME)
-    led.value = 0
 
 # --- main loop   ------------------------------------------------------------
 
 if CPU_FREQ:
   freqs = [CPU_FREQ_DEF,CPU_FREQ]
-  flip = int(alarm.sleep_memory[0] > 0)
   print(f"cpu-frequency: {microcontroller.cpu.frequency}")
 
 while True:
@@ -70,16 +81,22 @@ while True:
 
   # sleep
   if MODE == SPIN:
+    print(f"spinning for {INT_TIME}s ...")
     next = time.monotonic() + INT_TIME
     while time.monotonic() < next:
       continue
   elif MODE == SLEEP:
+    print(f"sleeping for {INT_TIME}s ...")
     time.sleep(INT_TIME)
   elif MODE == LIGHT_SLEEP:
+    print(f"light-sleeping for {INT_TIME}s ...")
     time_alarm = alarm.time.TimeAlarm(monotonic_time=time.monotonic()+INT_TIME)
     alarm.light_sleep_until_alarms(time_alarm)
+    WORK_TIME *= 1.5
   elif MODE == DEEP_SLEEP:
+    print(f"deep-sleeping for {INT_TIME}s ...")
     if CPU_FREQ:
       alarm.sleep_memory[0] = flip
     time_alarm = alarm.time.TimeAlarm(monotonic_time=time.monotonic()+INT_TIME)
     alarm.exit_and_deep_sleep_until_alarms(time_alarm)
+    WORK_TIME *= 1.5    # this should have no effect due to reset!
